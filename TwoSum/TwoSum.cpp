7f9755e1b362cd7e7a1a65b950ebd595e5244eaf
Vasu Patel <vasupatelworkmail@gmail.com>
2026-09-10 14:45:08 -0400

    code added to TwoSum

---
+++ TwoSum/TwoSum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class solution {
+
+public:
+
+    Vector<int> TwoSum(vector<int>& nums, int target) {
+
+        if (2 <= nums.length <= 10000) {
+
+            cout << "Error";
+            exit(0);
+
+
+        }
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+    }
+}
